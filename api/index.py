b4ce52d14083feff7ee1499b2269eb8c105941d9
Yan amorim <yan.amorim.silva@gmail.com>
2025-11-13 14:01:44 -0400

    first commit 6

--- api/index.py
+++ api/index.py
@@ -1,6 +1,10 @@
-def handler(request):
-    return {
-        "statusCode": 200,
-        "headers": {"Content-Type": "application/json"},
-        "body": '{"message": "Hello World"}'
-    }
+from http.server import BaseHTTPRequestHandler
+import json
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+        response = json.dumps({"message": "Hello World"})
+        self.wfile.write(response.encode())
